a77a687f51b52305fcc76ec9aac9008d9cb36d85
Adam Gardner <gardner.r.adam@gmail.com>
2023-04-15 17:22:49 -0500

    update ViewModel test to take id as int not long. Add null test for getting game details and test for getting correct game

--- app/src/test/java/com/gardner/adam_gardner_rm_interview/GameListViewModelTest.kt
+++ app/src/test/java/com/gardner/adam_gardner_rm_interview/GameListViewModelTest.kt
@@ -2,9 +2,9 @@ package com.gardner.adam_gardner_rm_interview
 
 import com.gardner.adam_gardner_rm_interview.data.ApiResult
 import com.gardner.adam_gardner_rm_interview.data.GameRepository
+import com.gardner.adam_gardner_rm_interview.data.GameViewModel
 import com.gardner.adam_gardner_rm_interview.data.remote.dto.Game
 import com.gardner.adam_gardner_rm_interview.data.remote.dto.ImageResult
-import com.gardner.adam_gardner_rm_interview.data.GameViewModel
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -16,6 +16,7 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -47,8 +48,8 @@ class GameListViewModelTest {
     fun `searchGames returns list of games`() = runBlocking {
         
         val expectedGames = listOf(
-            Game(1L, "Game 1", ImageResult("https://example.com/image1.jpg"), "Description 1"),
-            Game(2L, "Game 2", ImageResult("https://example.com/image2.jpg"), "Description 2")
+            Game(1, "Game 1", ImageResult("https://example.com/image1.jpg"), "Description 1"),
+            Game(2, "Game 2", ImageResult("https://example.com/image2.jpg"), "Description 2")
         )
         Mockito.`when`(repository.searchGames("API_KEY", "query"))
             .thenReturn(ApiResult.Success(expectedGames))
@@ -80,5 +81,24 @@ class GameListViewModelTest {
         
         assertTrue(actualGames.message.isNotEmpty())
         assertTrue(actualGames.message == expectedErrorMessage)
+    }
+    
+    @Test
+    fun `getGameById returns null if game is not found`() {
+        assertNull(viewModel.getGameById(1))
+    }
+    
+    @Test
+    fun `getGameById returns correct game`() = runBlocking {
+        val expectedGame =
+            Game(1, "Game 1", ImageResult("https://example.com/image1.jpg"), "Description 1")
+        val games = listOf(expectedGame)
+        Mockito.`when`(repository.searchGames("API_KEY", "query"))
+            .thenReturn(ApiResult.Success(games))
+        viewModel.searchGames("API_KEY", "query")
+        // Wait for the API to "complete"
+        delay(500)
+        val actualGame = viewModel.getGameById(1)
+        assertEquals(expectedGame, actualGame)
     }
 }
